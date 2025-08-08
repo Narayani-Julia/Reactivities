903c213483af869343521bdad9641605c838310a
Nara <jmnarayani@gmail.com>
2025-08-07 22:16:05 -0400

    added client to solution

--- Persistance/obj/Debug/net9.0/Persistance.AssemblyInfo.cs
+++ Persistance/obj/Debug/net9.0/Persistance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08439e8469f02776bbe37b05211c9f67b2d0d167")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
